916f5754798745cd68f4b049db7fa62061f886c4
Harshavardhan8693 <59202701+Harshavardhan8693@users.noreply.github.com>
2025-01-11 19:31:41 +0530

    Create Prpblem_69_Sqrt(x).java

---
+++ Prpblem_69_Sqrt(x).java
@@ -0,0 +1,58 @@
+/*
+Given a non-negative integer x, return the square root of x rounded down to the nearest integer. The returned integer should be non-negative as well.
+
+You must not use any built-in exponent function or operator.
+
+For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
+ 
+
+Example 1:
+
+Input: x = 4
+Output: 2
+Explanation: The square root of 4 is 2, so we return 2.
+Example 2:
+
+Input: x = 8
+Output: 2
+Explanation: The square root of 8 is 2.82842..., and since we round it down to the nearest integer, 2 is returned.
+ 
+
+Constraints:
+
+0 <= x <= 231 - 1
+*/
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class Solution {
+    public int mySqrt(int x) {
+        int odd =1,out=0,temp=x;
+        boolean flg=true;
+        if(temp == 0)
+            {
+                out = 0;
+                flg = false;
+            }
+            else if(temp == 1)
+            {
+                out = 1;
+                flg = false;
+            }
+            else{
+                while(flg == true)
+            {
+                temp = temp-odd;
+                if(temp < 0)
+                {
+                    flg=false;
+                }
+                else
+                {
+                    out=out+1;
+                }
+                odd=odd+2;
+            }
+            }
+        return out;
+    }
+}
